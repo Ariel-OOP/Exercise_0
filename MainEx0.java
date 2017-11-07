6f1d7b8a9063f964b0a7748708bcc57a0683cc04
Your Name <you@example.com>
2017-11-07 10:22:05 +0200

    Change name

--- MainEx0.java
+++ MainEx0.java
@@ -4,6 +4,6 @@ public class MainEx0 {
 		String pathFolder = "C:\\Users\\Moshe\\Desktop\\data\\27.10\\Lenovo";
 		
 		CSVReader sammeryCSV = new CSVReader(pathFolder,"wifiStrength");
-		System.out.println("hi moshe");
+		System.out.println("hi Nissan");
 	}
 }
