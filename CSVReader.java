4824637f90356d2a07641cb06c5c904430a1f1b7
Your Name <you@example.com>
2017-11-07 11:27:47 +0200

    test version

--- CSVReader.java
+++ CSVReader.java
@@ -16,16 +16,11 @@ public class CSVReader {
 
 	private String folderPath;
 	private String outputFileName;
-	private final int COLUMNS = 11;					// The number of columns
-//	File dir = new File(folderPath);		//	The current file
+	private final int COLUMNS = 11;			// The number of columns
+
 	boolean firstTimeHeader = true; 		//	Flag to check if the titles of columns in the csv file was written		
-	int lineCounter=0;						//	Number of strMatrix's lines
 	String[][] strMatrix; 					//	Matrix of parameters from txt file
-	//Scanner stdin;							//
-	String line_Text;						//	
 	String[][] headerMatrix;				//	Array of two lines of file's header 
-	String[] OnePoint = null;				//	Array that given from split(",") on each line of csv file
-//	String CurrentTime;						//	The time of a WIFI point
 	List<String[]> PointsOfOneMinute = new ArrayList<String[]>();	//	List of all WIFI points in given time
 
 
@@ -37,7 +32,7 @@ public class CSVReader {
 
 		folderPath = folderPathInput;
 		this.outputFileName = outputFileName;
-	
+
 		//Delete old csv file if exists
 		File fileToDelete = new File(outputFileName+".csv");
 		fileToDelete.delete();
@@ -50,8 +45,8 @@ public class CSVReader {
 	 */
 	public void ReadFilesInFolder(){
 		//Look through files in current folder
-		
-		Scanner stdin;							//
+
+		Scanner stdin;							
 		File dir = new File(folderPath);		//	The current file
 		for (File file : dir.listFiles()) {
 
@@ -64,12 +59,11 @@ public class CSVReader {
 					System.out.println("the file was not added to the csv file");
 					continue;
 				}
-
-				stdin = new Scanner(file);//Read the text from current file
+				//Read the text from current file
+				stdin = new Scanner(file);
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
-			//TODO I hope it goes here
 			ReadHeaderAndData(stdin);
 		}
 	}
@@ -79,38 +73,38 @@ public class CSVReader {
 	 * @param stdin
 	 */
 	public void ReadHeaderAndData(Scanner stdin){
-		
+
 		headerMatrix = new String[2][COLUMNS];
 		boolean hasCorrectHeader = true;	//If the method success to read the header and title than /*return true, else return false.*/
 		Line header = null, title = null;
+		String line_Text;						//	
+
 		//Split each line of csv file by ',' and insert to array
 
-		if(lineCounter >= 0)
-		{//if missing part or all from title, this file is invalid and the program continue to the next file in the folder
-			if(stdin.hasNext())
-			{
-				line_Text = stdin.nextLine();
-				headerMatrix[0] = line_Text.split(",");
-				header = new Line(line_Text);
-			}
-			else
-				hasCorrectHeader = false;
-			if(stdin.hasNext())
-			{
-				line_Text = stdin.nextLine();
-				headerMatrix[1] = line_Text.split(",");
-				title = new Line(line_Text);
-			}
-			else
-				hasCorrectHeader = false;
+		if(stdin.hasNext())
+		{
+			line_Text = stdin.nextLine();
+			headerMatrix[0] = line_Text.split(",");
+			header = new Line(line_Text);
 		}
+		else
+			hasCorrectHeader = false;
+		if(stdin.hasNext())
+		{
+			line_Text = stdin.nextLine();
+			headerMatrix[1] = line_Text.split(",");
+			title = new Line(line_Text);
+		}
+		else
+			hasCorrectHeader = false;
+
 		//check if in the title has all properties
-		
+
 		if(title != null && header != null && title.checkTitle() && header.checkHeader())
 			hasCorrectHeader = true;
 		else
 			hasCorrectHeader = false;
-		
+
 		if (hasCorrectHeader) {
 			FillAndSortMatrix(stdin);
 		}
@@ -120,12 +114,15 @@ public class CSVReader {
 
 	/**
 	 * 
+	 * @param stdin
 	 */
 	public void FillAndSortMatrix(Scanner stdin){
 
-		String currentLat = "", currentLon = "", currentAlt = "";
-		String currentTime;						//	The time of a WIFI point
-		
+		String currentLat = "", currentLon = "", currentAlt = "";	//	The lat, lon, alt of a WIFI point
+		String currentTime;											//	The time of a WIFI point
+		int lineCounter=0;											//	Number of strMatrix's lines
+		String[] OnePoint = null;									//	Array that given from split(",") on each line of csv file
+
 		if(stdin.hasNext())
 		{
 			//take the first line that is not title of the file.
@@ -137,18 +134,18 @@ public class CSVReader {
 			//Iterate through all the lines of the file
 
 			PointsOfOneMinute = new ArrayList<String[]>();
-			currentTime = OnePoint[3]; // the time property located 
-			currentLat = OnePoint[6];
-			currentLon = OnePoint[7];
-			currentAlt = OnePoint[8];
-			
+			currentTime = OnePoint[3];	// the time property located 
+			currentLat = OnePoint[6];	// the lat property located 
+			currentLon = OnePoint[7];	// the lon property located 
+			currentAlt = OnePoint[8];	// the alt property located 
+
 			PointsOfOneMinute.add(OnePoint);
 			lineCounter++;
 
 			while (stdin.hasNext()) {
-				
+
 				OnePoint = stdin.nextLine().split(",");
-				if(CheckIfInOneLat(currentLat,currentLon,currentAlt,currentTime,OnePoint))
+				if(CheckSameParameters(currentLat,currentLon,currentAlt,currentTime,OnePoint))
 				{
 					//Look for number of lines in current minute
 					lineCounter++;
@@ -170,10 +167,10 @@ public class CSVReader {
 				}
 			});
 			//fill matrix from list
-			
+
 			strMatrix = new String[lineCounter][COLUMNS]; //Makes string matrix for wifi parameters
 			strMatrix = PointsOfOneMinute.toArray(new String[lineCounter][11]);
-			
+
 			FileCreater();
 		}
 	}
@@ -212,27 +209,38 @@ public class CSVReader {
 			}
 			out.println(my_new_str);
 
-			lineCounter=0;
 		} catch (IOException e) {
 			//exception handling left as an exercise for the reader
 		}
 
-		PointsOfOneMinute = null;
-		lineCounter = 0;	
+		PointsOfOneMinute = null;	
 	}
 
-	private boolean CheckIfInOneLat(String currentLat, String currentLon, String currentAlt, String currentTime, String[] line) {
-		
-		//String[] arrayLine = line.split(",");
-		
+	/**
+	 * 
+	 * @param currentLat
+	 * @param currentLon
+	 * @param currentAlt
+	 * @param currentTime
+	 * @param line
+	 * @return
+	 */
+	private boolean CheckSameParameters(String currentLat, String currentLon, String currentAlt, String currentTime, String[] line) {
+
 		boolean latIsEqual = CheckIfInOneLat(currentLat,line[6]);
 		boolean lonIsEqual = CheckIfInOneLon(currentLon,line[7]);
 		boolean altIsEqual = CheckIfInOneAlt(currentAlt,line[8]);;
-		boolean timesIsEqual = CheckIfInOneLon(currentTime,line[3]);
+		boolean timesIsEqual = CheckIfInOneTime(currentTime,line[3]);
 
 		return latIsEqual && lonIsEqual && altIsEqual && timesIsEqual;
 	}
-
+	
+	/**
+	 * 
+	 * @param currentLat
+	 * @param string
+	 * @return
+	 */
 	private boolean CheckIfInOneLat(String currentLat, String string) {
 		boolean latIsEqual;
 
@@ -241,6 +249,12 @@ public class CSVReader {
 		return latIsEqual;
 	}
 	
+	/**
+	 * 
+	 * @param currentLon
+	 * @param string
+	 * @return
+	 */
 	private boolean CheckIfInOneLon(String currentLon, String string) {
 		boolean lonIsEqual;
 
@@ -248,7 +262,14 @@ public class CSVReader {
 
 		return lonIsEqual;
 	}
-
+	
+	
+	/**
+	 * 
+	 * @param currentAlt
+	 * @param string
+	 * @return
+	 */
 	private boolean CheckIfInOneAlt(String currentAlt, String string) {
 		boolean altIsEqual;
 
@@ -256,7 +277,7 @@ public class CSVReader {
 
 		return altIsEqual;
 	}
-	
+
 	/**
 	 * 
 	 * @param currentTime
@@ -267,55 +288,10 @@ public class CSVReader {
 
 		boolean timesIsEqual;
 
-		String[] Time1 = currentTime.split(":");
-		String[] Time2 = string.split(":");
-
-		timesIsEqual = Time1[0].equals(Time2[0]) && Time1[1].equals(Time2[1]);
+		timesIsEqual = currentTime.equals(string);
 
 		return timesIsEqual;
 	}
-
-
-
-//	/**
-//	 * 
-//	 * @param headerMatrix
-//	 * @return
-//	 */
-//	public static boolean fileVerifier(String[][] header_Matrix)
-//	{
-//		boolean correct_Header = true;
-//
-//		if(!(header_Matrix[0][0].contains("WigleWifi") && 
-//				header_Matrix[0][1].contains("appRelease") &&
-//				header_Matrix[0][2].contains("model") &&
-//				header_Matrix[0][3].contains("release") &&
-//				header_Matrix[0][4].contains("device") &&
-//				header_Matrix[0][5].contains("display") &&
-//				header_Matrix[0][6].contains("board") &&
-//				header_Matrix[0][7].contains("brand")))
-//		{
-//			correct_Header = false;
-//		}
-//
-//		if(!(header_Matrix[1][0].equals("MAC") && 
-//				header_Matrix[1][1].equals("SSID") &&
-//				header_Matrix[1][2].equals("AuthMode") &&
-//				header_Matrix[1][3].equals("FirstSeen") &&
-//				header_Matrix[1][4].equals("Channel") &&
-//				header_Matrix[1][5].equals("RSSI") &&
-//				header_Matrix[1][6].equals("CurrentLatitude") &&
-//				header_Matrix[1][7].equals("CurrentLongitude") &&
-//				header_Matrix[1][8].equals("AltitudeMeters") &&
-//				header_Matrix[1][9].equals("AccuracyMeters") &&
-//				header_Matrix[1][10].equals("Type")))
-//		{
-//			correct_Header = false;
-//		}
-//
-//		return correct_Header;
-//	}
-
 }
 
 
