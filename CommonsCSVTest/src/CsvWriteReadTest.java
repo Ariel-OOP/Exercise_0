a1c874e3a4a07a7d381b0e6716a876606e74a85f
NissanGoldberg <nissan.goldberg@gmail.com>
2017-11-17 12:49:59 +0200

    Iterates through csv files

--- CommonsCSVTest/src/CsvWriteReadTest.java
+++ CommonsCSVTest/src/CsvWriteReadTest.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * @author moshe and nissan
  *
@@ -8,16 +12,39 @@ public class CsvWriteReadTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
+		Scanner stdin;			
+		String folderPath="C:\\Users\\nissa\\Desktop\\Year 2\\OOP\\Programs\\27.10\\Lenovo";
+//		String folderPath="C:\\Users\\Moshe\\Desktop";
+		File dir = new File(folderPath);		//	The current file
+		for (File file : dir.listFiles()) {
+
+			stdin = null;	//Gave null so it would be initialized
+
+			try {
+				//Incorrect file type-reject
+				if (!(file.getName().toLowerCase().endsWith(".csv"))){
+					System.out.println(file.getName()+" is an incorrect file type in the folder");
+					System.out.println("the file was not added to the csv file");
+					continue;
+				}
+				//Read the text from current file
+				stdin = new Scanner(file);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			System.out.println("\nRead CSV file:");
+			CsvFileReader csvFileReader = new CsvFileReader();
+			csvFileReader.readCsvFile(file.toString());	
+		}
+
 		String fileName = "FromWIFIWigle.csv";
 		/*System.getProperty("user.home")+*/
-		
-//		System.out.println("Write CSV file:");
-//		CsvFileWriter.writeCsvFile(fileName);
-		
-		System.out.println("\nRead CSV file:");
-		CsvFileReader csvFileReader = new CsvFileReader();
-		csvFileReader.readCsvFile(fileName);
+
+		//		System.out.println("Write CSV file:");
+		//		CsvFileWriter.writeCsvFile(fileName);
+
+
 
 	}
 
